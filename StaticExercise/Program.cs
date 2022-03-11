43763b958c96ec74cd159f97263aa6fd5613a1d4
wewilliams9999 <williamswallise@gmail.com>
2022-03-10 20:31:57 -0600

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"Celsius: {celsius}");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(23);
+            Console.WriteLine($"Fahrenheit: {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double FahrenheitNumber)
+        {
+            
+            var result = (FahrenheitNumber - 32) * 5/9;
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double CelsiusNumber)
+        {
+            var result = CelsiusNumber * 9/5 + 32;
+            return result;
+        }
+    }
+}
